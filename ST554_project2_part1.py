48e86ff9cf089120887646379c2d8903e17af84d
fhzhkunming <46935214+fhzhkunming@users.noreply.github.com>
2026-03-15 11:29:47 -0400

    Add files via upload

--- ST554_project2_part1.py
+++ ST554_project2_part1.py
@@ -22,33 +22,110 @@ class SparkDataCheck:
     
     #==============================================================
     # Classmethod 1: create instance by reading a CSV file
-    @classmethod
-    # create method for creating an instance from a pandas dataframe
+    @classmethod    
     def from_csv(cls, spark, path):
         df = (spark.read
                    .format("csv")
                    .option("header", True)
                    .option("inferSchema", True)
+                   .option("sep", sep)
                    .load(path))
         return cls(df)
    
   
     #==============================================================
     # Classmethod 2: create instance from a pandas DataFrame
+    @classmethod
     def from_pandas(cls,spark, pandas_df):
         df = spark.createDataFrame(pandas_df)
         return cls(df)
     
+
     
-    
-    
-    
-    #############
-    # validation methods
-    
-    # create boolean column based on numberic bounds
-    def check_numeric_range(self, column: str, lower: str = None, upper: str = None):
+    # -------------------------------------------------------------------------------
+    # define a method to report min and max of a numeric coulumn supplied by the user
+    # -------------------------------------------------------------------------------
+    # import modules needed
+    from pyspark.sql.functions import min, max  
+    from pyspark.sql.types import NumericType   
+    from functools import reduce
+    import pandas as pd
+
+    def min_max(self, col = None, group = None):
         """
-         Append Boolean column indicating whether numeric values fall within bounds.
+        Report min and max for:
+        1. a user-supplied numeric column (grouped if provided)
+        2. or all numeric columns if no column is supplied (grouped if provided)
         """
+        #-------------------------------------
+        # scenario 1: User supplies a column
+        #-------------------------------------
+        if col is not None:
+            # check column exists
+            if col not in self.df.columns:
+                print(f"Column {col} don't exist.")
+                return None
+
+            # get the data type of the column
+            dtype = self.df.schema[col].dataType
+
+            # check if column is numeric
+            if not isinstance(dtype, NumericType):
+                print(f"Column '{col}' is not numeric.")
+                return None
+
+            # grouped version for a single numeric column
+            if group is not None:
+                return(
+                    self.df.groupBy(group)
+                           .agg(min(col).alias(f"{col}_min"),
+                               max(col).alias(f"{col}_max"))
+                )
+
+            # ungrouped version for a single numeric column      
+            return self.df.select(
+                min(col).alias(f"{col}_min"),
+                max(col).alias(f"{col}_max")
+            )
+
+        #----------------------------------------------------------------
+        # scenario 2: no column supplied: compute for all numeric columns
+        #----------------------------------------------------------------
+        # identify all numeric columns
+        numeric_cols = [
+            field.name
+            for field in self.df.schema.fields
+            if isinstance(field.dataType, NumericType)]
+
+        # if no numeric columns exist, print a message
+        if not numeric_cols:
+            print("No numeric columns found.")
+            return None
+
+        # grouped version for all numeric columns
+        if group is not None:
+            # setup an enpty list
+            dfs = []
+            # compute grouped min/max for each numeric column 
+            for c in numeric_cols:
+                df_c = (
+                    self.df.groupBy(group)
+                           .agg(min(c).alias(f"{c}_min"),
+                               max(c).alias(f"{c}_max"))
+                ).toPandas()
+                dfs.append(df_c)
+
+            # merge all grouped results into on DataFrame
+            merged = reduce(lambda left, right: pd.merge(left, right, on = group),dfs)
+            return merged
+
+        # ungrouped scenartion for all numeric columns
+        agg_exprs = []
+        for c in numeric_cols:
+            agg_exprs.extend([min(c).alisa(f"{c}_min"),
+                             max(c).alisa(f"{c}_max")])
+        # Return a DataFrame with all min/max values
+        return self.df.select(*agg_exprs)    
+
+
         
