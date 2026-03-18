261850dac9c1115c9f4ca6af026cec6cd4488711
fhzhkunming <46935214+fhzhkunming@users.noreply.github.com>
2026-03-18 00:04:18 -0400

    Add files via upload
    update methods

--- ST554_project2_part1.py
+++ ST554_project2_part1.py
@@ -1,3 +1,9 @@
+########################### 
+# Author: Hui Fang
+# Purpose: ST 554 Project 2
+# Date: 3/15/2026
+###########################
+
 """
 SparkDataCheck.py
 
@@ -10,31 +16,36 @@ from pyspark.sql import functions as F
 from functools import reduce
 from pyspark.sql.types import *
 import pandas as pd
+from pyspark.sql.types import NumericType
+from pyspark.sql.functions import col as spark_col
 
 class SparkDataCheck:
     """
-    A data qulity class for Spark SQL style data frames
+    A data quality class for Spark SQL style data frames
     """
     
     def __init__(self, df: DataFrame):
         # create a .df attribute
         self.df = df
     
-    #==============================================================
+    #------------------------------------------------------
     # Classmethod 1: create instance by reading a CSV file
+    #------------------------------------------------------
     @classmethod    
-    def from_csv(cls, spark, path):
+    def from_csv(cls, spark, path, sep = ";"):
         df = (spark.read
                    .format("csv")
                    .option("header", True)
                    .option("inferSchema", True)
                    .option("sep", sep)
-                   .load(path))
+                   .load(path)
+        )
         return cls(df)
    
   
-    #==============================================================
+    #-------------------------------------------------------
     # Classmethod 2: create instance from a pandas DataFrame
+    #-------------------------------------------------------
     @classmethod
     def from_pandas(cls,spark, pandas_df):
         df = spark.createDataFrame(pandas_df)
@@ -48,8 +59,7 @@ class SparkDataCheck:
     #---------------------------------------------------
     # 1.1 create boolean column based on numberic bounds
     #---------------------------------------------------
-    from pyspark.sql.functions import col as spark_col
-
+       
     def check_numeric_range(self, col: str, lower: float = None, upper: float = None):
         """
         Append a Boolean column indicating whether values in a numeric column
@@ -65,7 +75,7 @@ class SparkDataCheck:
             return self
 
         # -----------------------------------
-        # check if the columin is numeric
+        # check if the column is numeric
         #------------------------------------
         dtype = self.df.schema[col].dataType
         if not isinstance(dtype, NumericType):
@@ -80,18 +90,17 @@ class SparkDataCheck:
             return self
 
         #--------------------------------------------------------
-        # build the Boolean condition
-        # Spark automatically returns NULL when the input is NULL
+        # build the Boolean condition       
         #--------------------------------------------------------
         if lower is not None and upper is not None:
             # use Spark's between() when both bounds exist
-            condition = spark_col(col).between(lower, upper)
+            condition = spark_col(f"`{col}`").between(lower, upper)
         elif lower is not None:
             # only lower bound provided
-            condition = spark_col(col) >= lower
+            condition = spark_col(f"`{col}`") >= lower
         elif upper is not None:
             # only upper bound provided
-            condition = spark_col(col) <= upper
+            condition = spark_col(f"`{col}`") <= upper
 
         #---------------------------------
         # append Boolean column to dataframe
@@ -103,10 +112,7 @@ class SparkDataCheck:
     #----------------------------------------------------------------
     # 1.2 create a method checking values fall within a set of levels
     #----------------------------------------------------------------
-    # import modules needed
-    from pyspark.sql.functions import col as spark_col
-    from pyspark.sql.types import StringType
-
+      
     def check_value_levels(self, col: str, levels):
         """
         Check whether values in a string column fall within 
@@ -122,16 +128,16 @@ class SparkDataCheck:
             return self
 
         # -----------------------------------
-        # check if the columin is string
+        # check if the columin is string type
         #------------------------------------
         dtype = self.df.schema[col].dataType
         if not isinstance(dtype, StringType):
             print(f"Column '{col}' is not a string column.")
             return self
 
-
         # build the Boolean condition
         condition = spark_col(col).isin(levels)
+        
         # append the new Boolean column
         new_col_name = f"{col}_in_levels"
         self.df = self.df.withColumn(new_col_name, condition)
@@ -140,8 +146,7 @@ class SparkDataCheck:
     #----------------------------------------------------------------------
     # 1.3 create a method that checks if each value in a column is missing
     #----------------------------------------------------------------------
-    from pyspark.sql.functions import col as spark_col
-
+    
     def check_value_missing(self, col):
         """
         Check whether values in a given column are NULL.
@@ -154,8 +159,8 @@ class SparkDataCheck:
         if col not in self.df.columns:
             print(f"Column '{col}' does not exist.")
             return self
-
-        # build the Bollean contion
+       
+        # build the Boolean condition
         # .isNull() returns True, False, or NULL
         condition = spark_col(col).isNull()
 
@@ -171,19 +176,17 @@ class SparkDataCheck:
     #======================================
     
     # ----------------------------------------------------------------------------------
-    # 2.1 define a method to report min and max of a numeric coulumn supplied by the user
+    # 2.1 define a method to report min and max of a numeric column supplied by the user
     # ----------------------------------------------------------------------------------
-    # import modules needed
+    # import the module needed
     from pyspark.sql.functions import min, max  
-    from pyspark.sql.types import NumericType   
-    from functools import reduce
-    import pandas as pd
-
+    
     def min_max(self, col = None, group = None):
         """
         Report min and max for:
         1. a user-supplied numeric column (grouped if provided)
         2. or all numeric columns if no column is supplied (grouped if provided)
+        Returns a pandas DataFrame
         """
         #-------------------------------------
         # scenario 1: User supplies a column
@@ -191,29 +194,39 @@ class SparkDataCheck:
         if col is not None:
             # check column exists
             if col not in self.df.columns:
-                print(f"Column {col} don't exist.")
+                print(f"Column {col} does not exist.")
                 return None
 
-            # get the data type of the column
+            # check data type of the column
             dtype = self.df.schema[col].dataType
 
             # check if column is numeric
             if not isinstance(dtype, NumericType):
                 print(f"Column '{col}' is not numeric.")
                 return None
-
+            
+            # check group exists if provided
+            if group is not None and group not in self.df.columns:
+                print(f"Group column '{group}' does not exist.")
+                return None
+            
+            #---------------------------------------------
             # grouped version for a single numeric column
             if group is not None:
                 return(
                     self.df.groupBy(group)
-                           .agg(min(col).alias(f"{col}_min"),
-                               max(col).alias(f"{col}_max"))
+                           .agg(self.min(col).alias(f"{col}_min"),
+                                self.max(col).alias(f"{col}_max"))
+                           .toPandas()
                 )
 
+            #----------------------------------------------
             # ungrouped version for a single numeric column      
-            return self.df.select(
-                min(col).alias(f"{col}_min"),
-                max(col).alias(f"{col}_max")
+            return (
+                self.df.select(
+                    self.min(col).alias(f"{col}_min"),
+                    self.max(col).alias(f"{col}_max")
+                ).toPandas()
             )
 
         #----------------------------------------------------------------
@@ -229,37 +242,45 @@ class SparkDataCheck:
         if not numeric_cols:
             print("No numeric columns found.")
             return None
+        
+        # check group exists if provided
+        if group is not None and group not in self.df.columns:
+            print(f"Group column '{group}' does not exist.")
+            return None
 
+        #----------------------------------------------
         # grouped version for all numeric columns
         if group is not None:
-            # setup an enpty list
+            # setup an empty list
             dfs = []
             # compute grouped min/max for each numeric column 
             for c in numeric_cols:
                 df_c = (
                     self.df.groupBy(group)
-                           .agg(min(c).alias(f"{c}_min"),
-                               max(c).alias(f"{c}_max"))
+                           .agg(self.min(c).alias(f"{c}_min"),
+                                self.max(c).alias(f"{c}_max"))
                 ).toPandas()
                 dfs.append(df_c)
 
-            # merge all grouped results into on DataFrame
+            # merge all grouped results into one DataFrame
             merged = reduce(lambda left, right: pd.merge(left, right, on = group),dfs)
             return merged
 
-        # ungrouped scenartion for all numeric columns
+        #----------------------------------------------
+        # ungrouped scenario for all numeric columns
         agg_exprs = []
         for c in numeric_cols:
-            agg_exprs.extend([min(c).alisa(f"{c}_min"),
-                             max(c).alisa(f"{c}_max")])
+            agg_exprs.extend([
+                self.min(c).alias(f"{c}_min"),
+                self.max(c).alias(f"{c}_max")
+            ])
         # Return a DataFrame with all min/max values
-        return self.df.select(*agg_exprs)    
+        return self.df.select(*agg_exprs).toPandas()    
 
     #----------------------------------------------------------------------
     # 2.2 create a method that reports counts of one of two string columns
     #----------------------------------------------------------------------
-    from pyspark.sql.functions import col as spark_col
-
+    
     def count_column(self, col1: str, col2: str = None):
         """
         Report counts associated with one or two string columns.
@@ -282,7 +303,7 @@ class SparkDataCheck:
             return self
 
         # -----------------------------------
-        # check if columin1 is string
+        # check if column1 is string
         #------------------------------------
         dtype1 = self.df.schema[col1].dataType
         if not isinstance(dtype1, StringType):
